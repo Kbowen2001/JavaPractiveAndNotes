f3917654c4796665b5e01737612b1f3212243f02
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-04 12:00:33 -0700

    add statement

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 
 document.getElementById("demo1").innerHTML = 5 * 10;
 document.getElementById("demo2").innerHTML = "john" + " " + "Doe";
+
+//last line is showing a statement
+    let x, y, z;
+         x = 5;
+         y = 6;
+         z = x + y;
+         document.getElementById("demo4").innerHTML = "The value of z is" + z; 
